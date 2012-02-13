6099ea413f9aabdb9b00bdd043a29773016fe0a9
hippiehunter <hippiehunter@hh-mob.(none)>
2012-02-12 16:35:07 -0800

    migrated to new wrappers

--- test/clj_htmlunit/test/core.clj
+++ test/clj_htmlunit/test/core.clj
@@ -1,6 +1,29 @@
-(ns clj-html-scrapper.test.core
-  (:use [clj-html-scrapper.core] :reload-all)
+(ns clj-htmlunit.test.core
+  (:require [clj-htmlunit.html-element :as he])
+  (:require [clj-htmlunit.html-page :as hp])
+  (:require [clj-htmlunit.web-client :as wc])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-web-client
+  (is (wc/make-web-client 
+        :redirect? true 
+        :css? true) 
+      "creation of webclient failed"))
+
+(deftest test-get-html-page
+  (is (hp/get-page (wc/make-web-client
+                     :redirect? true
+                     :disable-logging? true)
+                   "http://www.google.com")))
+
+;;these tests rely on google staying the same
+;;thats a terrible idea
+(deftest test-html-page
+  (let [page (hp/get-page (wc/make-web-client
+                            :redirect? true
+                            :javascript? true
+                            :disable-logging? true)
+                          "http://www.google.com")]
+    (is (hp/document-element page))
+    (is (he/id (hp/element-by-id page "csi")) "csi")))
+      
